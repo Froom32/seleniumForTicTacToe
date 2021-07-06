4ce5da92a86f2b68512b0f43dd64fa5ccda428ef
mykhailo.bilyk <mykhailo.bilyk@visiquate.com>
2021-07-06 17:50:35 +0300

    Add autotests project

---
+++ tests/index.spec.js
@@ -0,0 +1,94 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+describe("Example.com", () => {
+  let driver;
+  
+  beforeAll(async function() {
+    driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('C:\\Users\\Mykhailo.bilyk\\Documents\\learningJS\\index.html');
+  });
+
+  afterAll(async function() {
+    await driver.quit();
+  });
+
+  beforeEach(async function() {
+    await click(driver, 'board_3');
+    await click(driver, 'btn-new-game');
+  });
+
+  async function click(driver, id) {
+    await driver.findElement(By.id(id)).click();
+  };
+
+  function findByID(driver, id) {
+    return driver.findElement(By.id(id));
+  };
+
+  it("X player should win by vertical line", async () => {
+    await click(driver, '00');
+    await click(driver, '01');
+    await click(driver, '10');
+    await click(driver, '02');
+    await click(driver, '20');
+    let modalWindow = await findByID(driver, 'content');
+    await expect(await modalWindow.getText()).toEqual("X Won!");
+  });
+
+  it("0 player should win by horizontal line", async () => {
+    await click(driver, '10');
+    await click(driver, '00');
+    await click(driver, '11');
+    await click(driver, '01');
+    await click(driver, '20');
+    await click(driver, '02');
+    let modalWindow = await findByID(driver, 'content');
+    await expect(await modalWindow.getText()).toEqual("0 Won!");
+  });
+
+  it("X player should win by diagonal line", async () => {
+    await click(driver, '00');
+    await click(driver, '01');
+    await click(driver, '11');
+    await click(driver, '02');
+    await click(driver, '22');
+    let modalWindow = await findByID(driver, 'content');
+    await expect(await modalWindow.getText()).toEqual("X Won!");
+  });
+
+  it("0 player should win by opposite diagonal line", async () => {
+    await click(driver, '00');
+    await click(driver, '02');
+    await click(driver, '10');
+    await click(driver, '11');
+    await click(driver, '22');
+    await click(driver, '20');
+    let modalWindow = await findByID(driver, 'content');
+    await expect(await modalWindow.getText()).toEqual("0 Won!");
+  });
+
+  it("There should be a draw", async () => {
+    await click(driver, '00');
+    await click(driver, '01');
+    await click(driver, '02');
+    await click(driver, '11');
+    await click(driver, '10');
+    await click(driver, '12');
+    await click(driver, '21');
+    await click(driver, '20');
+    await click(driver, '22');
+    let modalWindow = await findByID(driver, 'content');
+    await expect(await modalWindow.getText()).toEqual("Nobody Won!");
+  });
+
+  it("Players could not make one step twice in one box", async () => {
+    let err;
+    await click(driver, '00');
+    try{
+      await click(driver, '00');
+    } catch (e) {
+      err = e.message;
+    };
+    expect(err).toEqual("Err");
+  });
+});
