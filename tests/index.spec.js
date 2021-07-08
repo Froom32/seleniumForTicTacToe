b5d55fb8a0c4e5efd349422df53d313afc266527
mykhailo.bilyk <mykhailo.bilyk@visiquate.com>
2021-07-08 11:37:33 +0300

    Rename class Main to MainPage

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,80 +1,80 @@
-const {Main} = require('../lib/main');
+const {MainPage} = require('../lib/MainPage');
 
 describe("Example.com", () => {
-  const browser = new Main();
+  const mainPage = new MainPage();
   
   beforeAll(async function() {
-    await browser.get('https://Froom32.github.io');
+    await mainPage.get('https://Froom32.github.io');
   });
 
   afterAll(async function() {
-    await browser.close();
+    await mainPage.close();
   });
 
   beforeEach(async function() {
-    await browser.selectBoardSize('board_3');
+    await mainPage.selectBoardSize('board_3');
   });
 
   it("X player should win by vertical line", async () => {
-    await browser.clickByID('00');
-    await browser.clickByID('01');
-    await browser.clickByID('10');
-    await browser.clickByID('02');
-    await browser.clickByID('20');
-    let modalWindow = await browser.findByID('content');
+    await mainPage.clickByID('00');
+    await mainPage.clickByID('01');
+    await mainPage.clickByID('10');
+    await mainPage.clickByID('02');
+    await mainPage.clickByID('20');
+    let modalWindow = await mainPage.findByID('content');
     await expect(await modalWindow.getText()).toEqual("X Won!");
   });
 
   it("0 player should win by horizontal line", async () => {
-    await browser.clickByID('10');
-    await browser.clickByID('00');
-    await browser.clickByID('11');
-    await browser.clickByID('01');
-    await browser.clickByID('20');
-    await browser.clickByID('02');
-    let modalWindow = await browser.findByID('content');
+    await mainPage.clickByID('10');
+    await mainPage.clickByID('00');
+    await mainPage.clickByID('11');
+    await mainPage.clickByID('01');
+    await mainPage.clickByID('20');
+    await mainPage.clickByID('02');
+    let modalWindow = await mainPage.findByID('content');
     await expect(await modalWindow.getText()).toEqual("0 Won!");
   });
 
   it("X player should win by diagonal line", async () => {
-    await browser.clickByID('00');
-    await browser.clickByID('01');
-    await browser.clickByID('11');
-    await browser.clickByID('02');
-    await browser.clickByID('22');
-    let modalWindow = await browser.findByID('content');
+    await mainPage.clickByID('00');
+    await mainPage.clickByID('01');
+    await mainPage.clickByID('11');
+    await mainPage.clickByID('02');
+    await mainPage.clickByID('22');
+    let modalWindow = await mainPage.findByID('content');
     await expect(await modalWindow.getText()).toEqual("X Won!");
   });
 
   it("0 player should win by opposite diagonal line", async () => {
-    await browser.clickByID('00');
-    await browser.clickByID('02');
-    await browser.clickByID('10');
-    await browser.clickByID('11');
-    await browser.clickByID('22');
-    await browser.clickByID('20');
-    let modalWindow = await browser.findByID('content');
+    await mainPage.clickByID('00');
+    await mainPage.clickByID('02');
+    await mainPage.clickByID('10');
+    await mainPage.clickByID('11');
+    await mainPage.clickByID('22');
+    await mainPage.clickByID('20');
+    let modalWindow = await mainPage.findByID('content');
     await expect(await modalWindow.getText()).toEqual("0 Won!");
   });
 
   it("There should be a draw", async () => {
-    await browser.clickByID('00');
-    await browser.clickByID('01');
-    await browser.clickByID('02');
-    await browser.clickByID('11');
-    await browser.clickByID('10');
-    await browser.clickByID('12');
-    await browser.clickByID('21');
-    await browser.clickByID('20');
-    await browser.clickByID('22');
-    let modalWindow = await browser.findByID('content');
+    await mainPage.clickByID('00');
+    await mainPage.clickByID('01');
+    await mainPage.clickByID('02');
+    await mainPage.clickByID('11');
+    await mainPage.clickByID('10');
+    await mainPage.clickByID('12');
+    await mainPage.clickByID('21');
+    await mainPage.clickByID('20');
+    await mainPage.clickByID('22');
+    let modalWindow = await mainPage.findByID('content');
     await expect(await modalWindow.getText()).toEqual("Nobody Won!");
   });
 
   it("Players could not make one step twice in one box", async () => {
-    await browser.clickByID('00');
-    await browser.clickByID('00');
-    let box = await browser.findByID('00');
+    await mainPage.clickByID('00');
+    await mainPage.clickByID('00');
+    let box = await mainPage.findByID('00');
     await expect(await box.getText()).toEqual('X');
   });
 });
